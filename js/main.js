a0a1671dc2d40e83ff39c5f176f60f0c4d17f204
Lita Shin <160485490+laCasciaConst@users.noreply.github.com>
2024-04-29 11:23:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,107 +1,254 @@
 // VARIABLES GLOBALES
 
-
 // DOM READY
 $(document).ready(function () {
-    console.log("ready");
+  console.log("ready");
 
-    $('section').easyDrag({
-        'handle': '.desktop-folder__title-background',
-        'container': $('.main'),
-        cursor: 'move', // le curseur affiché au survol, mettre '' pour pas ne y toucher
-        ontop: true, // si l'élément draggé doit venir au 1er plan ou pas
-        clickable: true // si l'élément doit aussi être cliquable
-    });
+  updateElementWidth();
+  $(window).on("resize", updateElementWidth);
 
-    $('.about').on('click', function () {
-        $(".computer").attr("src", "./src/img/computer_click.svg");
-        $('#about').show();
-        $('#about').removeClass('hidden');
-        $('.abt').css({backgroundColor:"#0D0907", color:"#f5f5f5"});
-    });
+  function updateElementWidth() {
+    var windowWidth = $(window).width(); // 현재 창의 너비를 저장
+    if (windowWidth >= 800 && windowWidth < 1320) {
+      // 창 너비가 800 이상, 1320 미만인 경우에만 실행
+      var vw = windowWidth / 120;
+      var vh = $(window).height() / 100;
+      var rem = parseFloat($("html").css("font-size"));
 
-    $('.projects').on('click', function () {
-        $('#projects').show();
-        $('#projects').removeClass('hidden');
-        $(".memory1").attr("src", "./src/img/memory_click.svg");
-        $('.proj').css({backgroundColor:"#0D0907", color:"#f5f5f5"});
-    });
+      console.log("vw:", vw, "vh:", vh, "rem:", rem);
 
-    $('.posts').on('click', function () {
-        $(".write").attr("src", "./src/img/write_click.svg");
-        $('#posts').show();
-        $('#posts').removeClass('hidden');
-        $('.pst').css({backgroundColor:"#0D0907", color:"#f5f5f5"});
-    });
+      var width = (vw * vh * rem) / 3 / 6;
+      console.log("Calculated width:", width);
 
-    $('button.memory-close').on('click', function () {
-        console.log('click close memory');
-        // let parent = $(this).parent().parent()
-        $(this).parent().parent().addClass('hidden');
-        $(".memory1").attr("src", "./src/img/memory.svg");
-        $('.proj').css({backgroundColor:"#f5f5f5", color:"#0D0907"}).stop();
-    });
-
-    $('button.computer-close').on('click', function () {
-        console.log('click close computer');
-        $(this).parent().parent().addClass('hidden');
-        $(".computer").attr("src", "./src/img/computer.svg");
-        $('.abt').css({backgroundColor:"#f5f5f5", color:"#0D0907"}).stop();
-    });
-
-    $('button.post-close').on('click', function () {
-        console.log('click close write');
-        $(this).parent().parent().addClass('hidden');
-        $(".write").attr("src", "./src/img/write.svg");
-        $('.pst').css({backgroundColor:"#f5f5f5", color:"#0D0907"}).stop();
-    });
-
-    $('button.close').on('click', function () {
-        console.log('click close');
-        $(this).parent().parent().addClass('hidden');
-    });
-    
-    $(document).ready(function() {
-        $('section').click(function () {
-            $(this).css('z-index', '99');
-            // 클릭된 섹션의 .solid div를 보이게 설정
-            $(this).find('.solid').css('display', 'block');
-
-            $('section').not(this).each(function() {
-                $(this).css('z-index', '2');
-                $(this).find('.solid').css('display', 'none');
-            });
+      var images = $("li.first-nav__icon img");
+      // console.log("Number of images selected:", images.length);
+      images
+        .each(function () {
+          $(this).css({
+            width: width + "px",
+            height: "auto",
+          });
+          console.log(width);
+        })
+        .attr("src", function () {
+          return $(this).attr("src");
         });
-    }); 
+    } else {
+      console.log("The window width is outside the specified range.");
+    }
+  }
+
+  $(window).resize(function () {
+    console.log("Resized");
+  });
+
+  var zIndexCounter = 5;
+
+  $(".posts").on("click", function () {
+    $(".write").attr("src", "../src/img/write_click.svg");
+    $("#portfolio_intrd").css("z-index", "3");
+    if ($("#posts").hasClass("hidden")) {
+      $("#posts")
+        .removeClass("hidden")
+        .css("z-index", zIndexCounter + 9);
+      zIndexCounter++;
+    } else {
+      var currentZIndex = parseInt($("#posts").css("z-index"), 10);
+      $("#posts").css("z-index", currentZIndex + 9);
+    }
+    if (zIndexCounter > 97) {
+      zIndexCounter = 10;
+    }
+    $(".desktop-folder")
+      .not("#posts", "#portfolio_intrd")
+      .each(function () {
+        if (!$(this).hasClass("hidden")) {
+          $(this).css("z-index", zIndexCounter++);
+        }
+      });
+    $(".pst").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
+  });
+
+  $(".edit").on("click", function () {
+    $("#portfolio_intrd").css("z-index", "3");
+    if ($("#edit").hasClass("hidden")) {
+      $("#edit")
+        .removeClass("hidden")
+        .css("z-index", zIndexCounter + 9);
+      zIndexCounter++;
+    } else {
+      var currentZIndex = parseInt($("#edit").css("z-index"), 10);
+      $("#edit").css("z-index", currentZIndex + 9);
+    }
+    $(".desktop-folder")
+      .not("#edit", "#portfolio_intrd")
+      .each(function () {
+        if (!$(this).hasClass("hidden")) {
+          $(this).css("z-index", zIndexCounter++);
+        }
+      });
+  });
+
+  $(".portf22").on("click", function () {
+    $("#portfolio_intrd").css("z-index", "3");
+    $("#projects").css("z-index", "98");
+    if ($("#portf22").hasClass("hidden")) {
+      $("#portf22").removeClass("hidden").css("z-index", "99");
+
+      zIndexCounter++;
+    } else {
+      var currentZIndex = parseInt($("#portf22").css("z-index"), 10);
+      $("#portf22").css("z-index", currentZIndex + 9);
+    }
+
+    $(".desktop-folder")
+      .not("#portf22", "#portfolio_intrd", "#projects")
+      .each(function () {
+        if (!$(this).hasClass("hidden")) {
+          $(this).css("z-index", zIndexCounter++);
+        }
+      });
+  });
+
+  $(".projects").on("click", function () {
+    $(".memory1").attr("src", "../src/img/memory_click.svg");
+    $("#portfolio_intrd").css("z-index", "3");
+    if ($("#projects").hasClass("hidden")) {
+      $("#projects")
+        .removeClass("hidden")
+        .css("z-index", zIndexCounter + 9);
+      zIndexCounter++;
+    } else {
+      var currentZIndex = parseInt($("#projects").css("z-index"), 10);
+      $("#projects").css("z-index", currentZIndex + 9);
+    }
+    if (zIndexCounter > 97) {
+      zIndexCounter = 10;
+    }
+    $(".desktop-folder")
+      .not("#projects", "#portfolio_intrd")
+      .each(function () {
+        if (!$(this).hasClass("hidden")) {
+          $(this).css("z-index", zIndexCounter++);
+        }
+      });
+    $(".proj").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
+  });
+
+  $(".about").on("click", function () {
+    $(".computer").attr("src", "../src/img/computer_click.svg");
+    $("#portfolio_intrd").css("z-index", "3");
+    if ($("#about").hasClass("hidden")) {
+      $("#about")
+        .removeClass("hidden")
+        .css("z-index", zIndexCounter + 9);
+      zIndexCounter++;
+    } else {
+      var currentZIndex = parseInt($("#about").css("z-index"), 10);
+      $("#about").css("z-index", currentZIndex + 9);
+    }
+    if (zIndexCounter > 95) {
+      zIndexCounter = 10;
+    }
+    $(".desktop-folder")
+      .not("#about", "#portfolio_intrd")
+      .each(function () {
+        if (!$(this).hasClass("hidden")) {
+          $(this).css("z-index", zIndexCounter++);
+        }
+      });
+    $(".abt").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
+  });
+
+  $("button.memory-close").on("click", function () {
+    console.log("click close memory");
+    // let parent = $(this).parent().parent()
+    $(this).parent().parent().addClass("hidden");
+    $(".memory1").attr("src", "../src/img/memory.svg");
+    $(".proj").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
+  });
+
+  $("button.computer-close").on("click", function () {
+    console.log("click close computer");
+    $(this).parent().parent().addClass("hidden");
+    $(".computer").attr("src", "../src/img/computer.svg");
+    $(".abt").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
+  });
+
+  $("button.post-close").on("click", function () {
+    console.log("click close write");
+    $(this).parent().parent().addClass("hidden");
+    $(".write").attr("src", "../src/img/write.svg");
+    $(".pst").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
+  });
+
+  $("button.close").on("click", function () {
+    console.log("click close");
+    $(this).parent().parent().addClass("hidden");
+  });
+
+  $("section").click(function (event) {
+    $(this).css("z-index", "99");
+    $(this).find(".solid").css("display", "block");
+  
+    $("section").not(this).each(function () {
+      $(this).css("z-index", "5");
+      $(this).find(".solid").css("display", "none");
+    });
+  
+    if ($(event.target).closest(".portf22").length > 0) {
+      $("#portf22").css("z-index", "101"); 
+    }
+  });
+  
+
+  $("section").easyDrag({
+    handle: ".desktop-folder__title",
+    container: $(".main"),
+    cursor: "move", // le curseur affiché au survol, mettre '' pour pas ne y toucher
+    ontop: true, // si l'élément draggé doit venir au 1er plan ou pas
+    clickable: true, // si l'élément doit aussi être cliquable
+  });
+
+  $("section").on("dragstart", function () {
+    $(this).css("z-index", 999);
+  });
+  $("section").on("dragend", function () {
+    $(this).css("z-index", "");
+  });
 });
 
 (function () {
-    //Functions that handle the clock
-    //Change date into correct apple format
-    function formatDate(day, hour, min) {
-        var parseHour = String(hour).padStart(2, '0'),
-            parseMin = String(min).padStart(2, '0');
-        return day + ' ' + parseHour + ':' + parseMin;
-    }
+  //Functions that handle the clock
+  //Change date into correct apple format
+  function formatDate(day, hour, min) {
+    var parseHour = String(hour).padStart(2, "0"),
+      parseMin = String(min).padStart(2, "0");
+    return day + " " + parseHour + ":" + parseMin;
+  }
 
-    var timeout; // 전역 변수로 timeout 선언
+  var timeout; // 전역 변수로 timeout 선언
 
-    function updateTime() {
-        var d = new Date(),
-            days = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'];
-        document.querySelector('.time').innerHTML = formatDate(days[d.getDay()], d.getHours(), d.getMinutes());
-        //Check every half a second if the time has changed
-        clearTimeout(timeout); // 이전 타이머 해제
-        timeout = setTimeout(updateTime, 500);
-    }
-    updateTime();
+  function updateTime() {
+    var d = new Date(),
+      days = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
+    document.querySelector(".time").innerHTML = formatDate(
+      days[d.getDay()],
+      d.getHours(),
+      d.getMinutes()
+    );
+    //Check every half a second if the time has changed
+    clearTimeout(timeout); // 이전 타이머 해제
+    timeout = setTimeout(updateTime, 500);
+  }
+  updateTime();
 })();
 
-$('.header__item').mouseenter(function () {
-    $(this).children('.lnb').stop().show().stop();
+$(".header__item").mouseenter(function () {
+  $(this).children(".lnb").stop().show().stop();
 });
-$('.header__item').mouseleave(function () {
-    $(this).children('.lnb').stop().hide().stop();
+$(".header__item").mouseleave(function () {
+  $(this).children(".lnb").stop().hide().stop();
 });
 
 // FUNCTIONS EXTERNES
