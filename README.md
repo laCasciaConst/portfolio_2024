1c57184f7b21873dc4f4732c7c1ba55e400e74f9
Lita Shin <160485490+laCasciaConst@users.noreply.github.com>
2024-12-03 14:26:32 +0100

    updated
    refactoring js code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # portfolio_2024
+
+* post 아이콘 추가 및 포스트 2~3개, portfolio data-tip 추가, edit 창 수정, new 창 닫기, ipad에서 css 깨짐 해결, 1360px 이상 디자인. 
+
+* 나중에해도됨 - 모바일 간단 지원, meta 요소 카톡에서 확인 및 추가, 2024 포트폴리오 추가.
--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
   $(window).on("resize", updateElementWidth);
 
   function updateElementWidth() {
-    var windowWidth = $(window).width(); // 현재 창의 너비를 저장
+    var windowWidth = $(window).width(); // 너비 저장
     var vw = windowWidth / 120;
     var vh = $(window).height() / 100;
     var rem = parseFloat($("html").css("font-size"));
-    var width = 0; // width 변수를 초기화
+    var width = 0; // 초기화
     var images = $("li.first-nav__icon img");
 
     console.log(
@@ -102,164 +102,111 @@ $(document).ready(function () {
 
   var zIndexCounter = 5;
 
-  $(".posts").on("click", function () {
-    $(".write").attr("src", "./src/img/write_click.svg");
-    $("#portfolio_intrd").css("z-index", "3");
-    if ($("#posts").hasClass("hidden")) {
-      $("#posts")
-        .removeClass("hidden")
-        .css("z-index", zIndexCounter + 9);
-      zIndexCounter++;
-    } else {
-      var currentZIndex = parseInt($("#posts").css("z-index"), 10);
-      $("#posts").css("z-index", currentZIndex + 9);
-    }
-    if (zIndexCounter > 97) {
-      zIndexCounter = 10;
-    }
-    $(".desktop-folder")
-      .not("#posts", "#portfolio_intrd")
-      .each(function () {
-        if (!$(this).hasClass("hidden")) {
-          $(this).css("z-index", zIndexCounter++);
-        }
-      });
-    $(".pst").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
-  });
+  function handleFolderClick(selector, imgSelector, imgClickedSrc, colorClass) {
+    $(selector).on("click", function () {
+      if (imgSelector && imgClickedSrc) {
+        $(imgSelector).attr("src", imgClickedSrc); 
+      }
 
-  $(".edit").on("click", function () {
-    $("#portfolio_intrd").css("z-index", "3");
-    if ($("#edit").hasClass("hidden")) {
-      $("#edit")
-        .removeClass("hidden")
-        .css("z-index", zIndexCounter + 9);
-      zIndexCounter++;
-    } else {
-      var currentZIndex = parseInt($("#edit").css("z-index"), 10);
-      $("#edit").css("z-index", currentZIndex + 9);
-    }
-    $(".desktop-folder")
-      .not("#edit", "#portfolio_intrd")
-      .each(function () {
-        if (!$(this).hasClass("hidden")) {
-          $(this).css("z-index", zIndexCounter++);
-        }
-      });
-  });
+      $("#portfolio_intrd").css("z-index", "3");
 
-  $(".portf22").on("click", function () {
-    $("#portfolio_intrd").css("z-index", "3");
-    $("#projects").css("z-index", "98");
-    if ($("#portf22").hasClass("hidden")) {
-      $("#portf22").removeClass("hidden").css("z-index", "99");
+      if ($(selector).hasClass("hidden")) {
+        $(selector)
+          .removeClass("hidden")
+          .css("z-index", zIndexCounter + 9);
+        zIndexCounter++;
+      } else {
+        var currentZIndex = parseInt($(selector).css("z-index"), 10);
+        $(selector).css("z-index", currentZIndex + 9);
+      }
 
-      zIndexCounter++;
-    } else {
-      var currentZIndex = parseInt($("#portf22").css("z-index"), 10);
-      $("#portf22").css("z-index", currentZIndex + 9);
-    }
+      if (zIndexCounter > 97) {
+        zIndexCounter = 10;
+      }
 
-    $(".desktop-folder")
-      .not("#portf22", "#portfolio_intrd", "#projects")
-      .each(function () {
-        if (!$(this).hasClass("hidden")) {
-          $(this).css("z-index", zIndexCounter++);
-        }
-      });
-  });
-
-  $(".projects").on("click", function () {
-    $(".memory1").attr("src", "./src/img/memory_click.svg");
-    $("#portfolio_intrd").css("z-index", "3");
-    if ($("#projects").hasClass("hidden")) {
-      $("#projects")
-        .removeClass("hidden")
-        .css("z-index", zIndexCounter + 9);
-      zIndexCounter++;
-    } else {
-      var currentZIndex = parseInt($("#projects").css("z-index"), 10);
-      $("#projects").css("z-index", currentZIndex + 9);
-    }
-    if (zIndexCounter > 97) {
-      zIndexCounter = 10;
-    }
-    $(".desktop-folder")
-      .not("#projects", "#portfolio_intrd")
-      .each(function () {
-        if (!$(this).hasClass("hidden")) {
-          $(this).css("z-index", zIndexCounter++);
-        }
-      });
-    $(".proj").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
-  });
-
-  $(".about").on("click", function () {
-    $(".computer").attr("src", "./src/img/computer_click.svg");
-    $("#portfolio_intrd").css("z-index", "3");
-    if ($("#about").hasClass("hidden")) {
-      $("#about")
-        .removeClass("hidden")
-        .css("z-index", zIndexCounter + 9);
-      zIndexCounter++;
-    } else {
-      var currentZIndex = parseInt($("#about").css("z-index"), 10);
-      $("#about").css("z-index", currentZIndex + 9);
-    }
-    if (zIndexCounter > 95) {
-      zIndexCounter = 10;
-    }
-    $(".desktop-folder")
-      .not("#about", "#portfolio_intrd")
-      .each(function () {
-        if (!$(this).hasClass("hidden")) {
-          $(this).css("z-index", zIndexCounter++);
-        }
-      });
-    $(".abt").css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
-  });
-
-  $("button.memory-close").on("click", function () {
-    console.log("click close memory");
-    // let parent = $(this).parent().parent()
-    $(this).parent().parent().addClass("hidden");
-    $(".memory1").attr("src", "./src/img/memory.svg");
-    $(".proj").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
-  });
-
-  $("button.computer-close").on("click", function () {
-    console.log("click close computer");
-    $(this).parent().parent().addClass("hidden");
-    $(".computer").attr("src", "./src/img/computer.svg");
-    $(".abt").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
-  });
-
-  $("button.post-close").on("click", function () {
-    console.log("click close write");
-    $(this).parent().parent().addClass("hidden");
-    $(".write").attr("src", "./src/img/write.svg");
-    $(".pst").css({ backgroundColor: "#f5f5f5", color: "#0D0907" }).stop();
-  });
-
-  $("button.close").on("click", function () {
-    console.log("click close");
-    $(this).parent().parent().addClass("hidden");
-  });
-
-  $("section").click(function (event) {
-    $(this).css("z-index", "99");
-    $(this).find(".solid").css("display", "block");
-
-    $("section")
-        .not(this)
+      $(".desktop-folder")
+        .not(selector, "#portfolio_intrd")
         .each(function () {
-            $(this).css("z-index", "5");
-            $(this).find(".solid").css("display", "none");
+          if (!$(this).hasClass("hidden")) {
+            $(this).css("z-index", zIndexCounter++);
+          }
         });
 
+      if (colorClass) {
+        $(".pst, .dft, .proj, .abt").css({ backgroundColor: "", color: "" }); // 초기화
+        $(colorClass).css({ backgroundColor: "#0D0907", color: "#f5f5f5" });
+      }
+    });
+  }
+
+  handleFolderClick("#posts", ".write", "./src/img/write_click.svg", ".pst");
+  handleFolderClick("#edit", null, null, null);
+  handleFolderClick("#portf22", null, null, null);
+  handleFolderClick(
+    "#projects",
+    ".memory1",
+    "./src/img/memory_click.svg",
+    ".proj"
+  );
+  handleFolderClick(
+    "#about",
+    ".computer",
+    "./src/img/computer_click.svg",
+    ".abt"
+  );
+
+  function handleCloseButton(buttonSelector, imgSelector, imgSrc, colorClass) {
+    $(buttonSelector).on("click", function () {
+      console.log(`click close ${buttonSelector}`);
+      $(this).parent().parent().addClass("hidden"); 
+
+      if (imgSelector && imgSrc) {
+        $(imgSelector).attr("src", imgSrc); 
+      }
+
+      if (colorClass) {
+        $(colorClass)
+          .css({ backgroundColor: "#f5f5f5", color: "#0D0907" })
+          .stop(); 
+      }
+    });
+  }
+
+  handleCloseButton(
+    "button.memory-close",
+    ".memory1",
+    "./src/img/memory.svg",
+    ".proj"
+  );
+  handleCloseButton(
+    "button.computer-close",
+    ".computer",
+    "./src/img/computer.svg",
+    ".abt"
+  );
+  handleCloseButton(
+    "button.post-close",
+    ".write",
+    "./src/img/write.svg",
+    ".pst"
+  );
+  handleCloseButton("button.close", null, null, null);
+
+  $("section").click(function (event) {
+    $(this).css("z-index", "99"); 
+    $(this).find(".solid").css("display", "block"); 
+
+    $("section")
+      .not(this) 
+      .each(function () {
+        $(this).css("z-index", "5"); 
+        $(this).find(".solid").css("display", "none"); 
+      });
+
     if ($(event.target).closest(".portf22").length > 0) {
-        $("#portf22").css("z-index", "101");
+      $("#portf22").css("z-index", "101");
     }
-});
+  });
 
   $("section").easyDrag({
     handle: ".desktop-folder__title",
@@ -268,19 +215,19 @@ $(document).ready(function () {
     ontop: true, // si l'élément draggé doit venir au 1er plan ou pas
     clickable: true, // si l'élément doit aussi être cliquable
   });
-  
+
   $(".desktop-folder__title").on("dragstart", function (event) {
     var $parentSection = $(this).parent();
     $parentSection.data("original-zindex", $parentSection.css("z-index"));
     $parentSection.css("z-index", 999);
-    event.stopPropagation();  // 중요: 이벤트 전파 중지
-});
+    event.stopPropagation(); // 중요: 이벤트 전파 중지
+  });
 
-$(".desktop-folder__title").on("dragend", function (event) {
+  $(".desktop-folder__title").on("dragend", function (event) {
     var $parentSection = $(this).parent();
     $parentSection.css("z-index", $parentSection.data("original-zindex"));
-    event.stopPropagation();  // 중요: 이벤트 전파 중지
-});
+    event.stopPropagation(); // 중요: 이벤트 전파 중지
+  });
 
   $("[data-tip]")
     .mouseenter(function (e) {
@@ -289,8 +236,8 @@ $(".desktop-folder__title").on("dragend", function (event) {
         .css({
           position: "absolute",
           display: "block",
-          left: e.pageX + 10 + "px", 
-          top: e.pageY + 10 + "px", 
+          left: e.pageX + 10 + "px",
+          top: e.pageY + 10 + "px",
         });
     })
     .mousemove(function (e) {
@@ -300,7 +247,7 @@ $(".desktop-folder__title").on("dragend", function (event) {
       });
     })
     .mouseout(function () {
-      $("#tooltip").hide(); 
+      $("#tooltip").hide();
     });
 });
 
